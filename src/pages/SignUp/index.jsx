51ef0d75fbc6c5349fa3c56599c5cee1270960df
joao <joaovitorsantossouza937@gmail.com>
2026-07-28 17:28:51 -0300

    feat: creates the registration page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,8 +1,33 @@
+import logo from "../../assets/images/logo.png"
+import { Link } from "react-router-dom"
+import { useState } from "react"
+const SignUp = () => {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-const  SignUp = () => {
-    return(
-        <div>
-            <h1>Página de cadastro</h1>
+    return (
+        <div className="container-center">
+            <div className="login">
+                <div className="login-area">
+                    <img src={logo} alt="Logo do sistema de chamados " />
+                </div>
+
+                <form>
+                    <h1>Cadastrar nova conta</h1>
+                    <input type="text" placeholder="Seu nome"
+                        value={name} onChange={(e) => setName(e.target.value)}
+                    />
+                    <input type="text" placeholder="email@email.com"
+                        value={email} onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input type="text" placeholder="Digite sua senha"
+                        value={password} onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button type="submit" >Cadastrar</button>
+                </form>
+                <Link to="/">Já possui uma conta? Faça login</Link>
+            </div>
         </div>
     )
 }

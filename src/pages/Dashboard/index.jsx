43dfa8d8e971199ddbf2bb1bb9d39ddc9eae35e4
joao <joaovitorsantossouza937@gmail.com>
2026-08-16 10:29:40 -0300

    feat: creates the dashboard page

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,9 +1,72 @@
+import { AuthContext } from "../../Contexts/auth"
+import { useContext } from "react"
+import Header from "../../components/Header"
+import "./dashboard.css"
+import Title from "../../components/Title"
+import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from "react-icons/fi"
+import { Link } from "react-router-dom"
 
 
 const Dashboard = () => {
+    const { logout } = useContext(AuthContext)
     return (
         <div>
-            <h1>página de Dashboard</h1>
+            <Header />
+            <div className="content">
+                <Title name="Tichtks">
+                    <FiMessageSquare size={25} />
+                </Title>
+
+                <>
+                    <Link to="/new " className="new">
+                        <FiPlus color="#FFF" size={25} />
+                        Novo Chamado
+                    </Link>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">Cliente</th>
+                                <th scope="col">Assunto</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Cadastrado em</th>
+                                <th scope="col">#</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-label="Cliente">Mecador Esquina</td>
+                                <td data-label="Assunto">Suporte</td>
+                                <td data-label="Status">Em Aberto</td>
+                                <td data-label="Cadastrado">12/09/2026</td>
+                                <td data-label="#">
+                                    <button className="action" style={{ backgroundColor: "#3583f6" }}>
+                                        <FiSearch color="#FFF" size={25} />
+                                    </button>
+                                    <button className="action" style={{ backgroundColor: "#f6a935" }}>
+                                        <FiEdit2 color="#FFF" size={25} />
+                                    </button>
+                                </td>
+                            </tr>
+
+
+                            <tr>
+                                <td data-label="Cliente">Informatica Tech</td>
+                                <td data-label="Assunto">Suporte</td>
+                                <td data-label="Status">Em Aberto</td>
+                                <td data-label="Cadastrado">12/09/2026</td>
+                                <td data-label="#">
+                                    <button className="action" style={{ backgroundColor: "#3583f6" }}>
+                                        <FiSearch color="#FFF" size={25} />
+                                    </button>
+                                    <button className="action" style={{ backgroundColor: "#f6a935" }}>
+                                        <FiEdit2 color="#FFF" size={25} />
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </>
+            </div>
         </div>
     )
 }
